06107f44e8bfe32b79a583b6461070999e906dd4
ViniDevBR <vinigtr386@gmail.com>
2022-08-14 15:15:10 -0300

    :fire: New function

--- menu.js
+++ menu.js
@@ -1,6 +1,14 @@
+const body = document.querySelector('body')
 const menu = document.querySelector(".menu")
 const navOn = document.querySelector("nav")
 
 menu.addEventListener('click', () =>{
   navOn.classList.toggle('on')
+ 
+  const navContainsON = navOn.classList.contains('on')
+  if(navContainsON === true){
+    body.classList.toggle('menuClose')
+  } else {
+    body.classList.toggle('menuClose')
+  }
 })
