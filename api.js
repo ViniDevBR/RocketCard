cbd79bda3d58129032141ae55d99fdfabba8562a
ViniDevBR <vinigtr386@gmail.com>
2022-08-14 11:36:50 -0300

    :fire: Ready

--- api.js
+++ api.js
@@ -7,13 +7,17 @@ function getUser(){
   .then(data => {
     userPhoto.src = data.avatar_url
     userName.textContent = data.login
-      userFollowers.textContent =`${data.followers} Followers`
+      userFollowers.textContent =`${data.followers} 
+                                  ${data.followers === 1 ? 'Follower' : 'Followers'}`
+
       userFollowing.textContent = `${data.following} Following`
-      userRepo.textContent =  `${data.public_repos} Repositories`
-      userCompany.textContent = data.company
-      userLocation.textContent = data.location
+
+      userRepo.textContent =  `${data.public_repos} 
+                               ${data.public_repos === 1 ? 'Repositorie' : 'Repositories'}`
+
+      userCompany.textContent = `${data.company === null ? 'Unknow' : data.company}`
+
+      userLocation.textContent = `${data.location === null ? 'Unknow' : data.location}`
     })
     .catch(error => console.error(error))
 }
-
-
